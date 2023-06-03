6f6c82e462b72fa3bb28ab26771762f66dbfc4ae
ceailo <s46390@student.umt.edu.my>
2023-06-03 21:16:56 +0800

    first commit

---
+++ build.zig
@@ -0,0 +1,103 @@
+const std = @import("std");
+
+const vkgen = @import("libs/vulkan-binding//index.zig");
+
+pub const ShaderCompileStep = vkgen.ShaderCompileStep;
+pub const VkGenerateStep = vkgen.VkGenerateStep;
+
+// Although this function looks imperative, note that its job is to
+// declaratively construct a build graph that will be executed by an external
+// runner.
+pub fn build(b: *std.Build) void {
+    // Standard target options allows the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
+    const target = b.standardTargetOptions(.{});
+
+    // Standard optimization options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
+    // set a preferred release mode, allowing the user to decide how to optimize.
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "pr_060323",
+        // In this case the main source file is merely a path, however, in more
+        // complicated build scripts, this could be a generated file.
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // This declares intent for the executable to be installed into the
+    // standard location when the user invokes the "install" step (the default
+    // step when running `zig build`).
+    b.installArtifact(exe);
+
+    // This *creates* a Run step in the build graph, to be executed when another
+    // step is evaluated that depends on it. The next line below will establish
+    // such a dependency.
+    const run_cmd = b.addRunArtifact(exe);
+
+    // By making the run step depend on the install step, it will be run from the
+    // installation directory rather than directly from within the cache directory.
+    // This is not necessary, however, if the application depends on other installed
+    // files, this ensures they will be present and in the expected location.
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build run`
+    // This will evaluate the `run` step rather than the default, which is "install".
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    // Creates a step for unit testing. This only builds the test executable
+    // but does not run it.
+    const unit_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+
+    // Similar to creating the run step earlier, this exposes a `test` step to
+    // the `zig build --help` menu, providing a way for the user to request
+    // running the unit tests.
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_unit_tests.step);
+
+    //
+    const vk_xml_path: ?[]const u8 = b.option([]const u8, "registry", "Override the path to the Vulkan registry");
+
+    // using the package manager, this artifact can be obtained by the user
+    // through `b.dependency(<name in build.zig.zon>, .{}).artifact("generator")`.
+    // with that, the user need only `.addArg("path/to/vk.xml")`, and then obtain
+    // a file source to the generated code with `.addOutputArg("vk.zig")`
+    const generator_exe = b.addExecutable(.{
+        .name = "vulkan-binding-gen",
+        .root_source_file = .{ .path = "libs/vulkan-binding/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(generator_exe);
+
+    // or they can skip all that, and just make sure to pass `.registry = "path/to/vk.xml"` to `b.dependency`,
+    // and then obtain the module directly via `.module("vulkan-zig")`.
+    if (vk_xml_path) |path| {
+        const generate_cmd = b.addRunArtifact(generator_exe);
+
+        if (!std.fs.path.isAbsolute(path)) @panic("Make sure to assign an absolute path to the `registry` option (see: std.Build.pathFromRoot).\n");
+        generate_cmd.addArg(path);
+
+        _ = b.addModule("vulkan-zig", .{
+            .source_file = generate_cmd.addOutputFileArg("vk.zig"),
+        });
+    }
+}
